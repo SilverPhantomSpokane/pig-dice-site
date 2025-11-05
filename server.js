f410a5ab6fb317025f563a53d1ebe01d5786d665
Mariia Baranova <mariiabaranova@Mariias-MacBook-Air.local>
2025-11-04 22:45:39 -0800

    form

--- server.js
+++ server.js
@@ -31,13 +31,15 @@ app.post("/send", async (req, res) => {
   const { email, message } = req.body;
 
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+   const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // SSL
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: `"Pig Dice Support" <${process.env.EMAIL_USER}>`,
